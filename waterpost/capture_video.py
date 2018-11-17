8cb3b3ed566ca337c8742cc4a4cc27e71131e7ff
akivab <akiva.bamberger@gmail.com>
2018-11-16 19:39:32 -0800

    use camera of system

--- waterpost/capture_video.py
+++ waterpost/capture_video.py
@@ -6,6 +6,7 @@ import tempfile
 from subprocess import call
 
 
+VIDEO_CAMERA = 1
 class VideoCapture():
     def __init__(self):
         self.maxRecordingTime = 60
@@ -24,14 +25,13 @@ class VideoCapture():
         if self.is_recording():
             print 'already recording a video'
             return -1
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(VIDEO_CAMERA)
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
         self.vout = cv2.VideoWriter()
         size = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
                 int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
         self.largeVideoFile = tempfile.NamedTemporaryFile(suffix=".mp4").name
-
         success = self.vout.open(self.largeVideoFile, fourcc, 30.0, size, True)
         if not success:
             print 'couldn\'t open file'
@@ -64,9 +64,8 @@ class VideoCapture():
         self.cap.release()
         self.vout.release()
         cv2.destroyAllWindows()
-        call(['ffmpeg', '-i', self.largeVideoFile, '-filter:v', 'scale=420:-1,setpts=0.25*PTS', compressedFile])
+        call(['ffmpeg', '-i', self.largeVideoFile, '-filter:v', 'scale=420:-1,setpts=0.5*PTS', compressedFile])
         self.temporaryFile = compressedFile
-
 
 if __name__ == '__main__':
     vid = VideoCapture()
